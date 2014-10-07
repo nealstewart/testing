8d78e2c9a1822dfbebf0968ba090133cbab2b85a
Neal Stewart <nes@tradeshift.com>
2014-10-07 15:45:34 +0200

    Added first tests for the view document lines file

--- spec/view_spec.js
+++ spec/view_spec.js
@@ -1,16 +1,39 @@
 var viewDocumentLines = require('../src/view_document_lines');
 
+var talk = require('../lib/talk');
+
+function MockScope() {
+}
+
 describe('viewDocumentLines', function() {
-	function MockScope() {
-	}
+
 	describe('controller', function() {
 		beforeEach(function() {
 			this.mockScope = new MockScope();
 
+			spyOn(talk, 'subscribe').andCallThrough();
+			spyOn(talk, 'broadcast').andCallThrough();
+
 			viewDocumentLines.onscope(this.mockScope);
 		});
 
-		it('does something', function() {
+		afterEach(function() {
+			talk.unsubscribeAll();
+		});
+
+		it('subscribes to talk', function() {
+			expect(talk.subscribe).toHaveBeenCalledWith(
+				'ap-line-matching-groups',
+				jasmine.any(Function)
+			);
+
+			expect(talk.subscribe).toHaveBeenCalledWith(
+				'ap-line-matching-error',
+				jasmine.any(Function)
+			);
+		});
+
+		it('initializes the view when a broadcast occurs', function() {
 		});
 	});
 });
